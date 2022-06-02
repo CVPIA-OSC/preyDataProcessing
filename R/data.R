a22176c9d10365e12c1084dca8b8748729e7dcaf
MaddeeRubenson <rubenson.m@gmail.com>
2022-06-02 15:13:23 -0700

    adding package start

---
+++ R/data.R
@@ -0,0 +1,37 @@
+#' Aggregated Prey Density Data from Multiple Authors and Across Watersheds
+#'
+#' @format a dataframe
+#'
+#' @details Datasets included in final dataset are:
+#'
+#'
+#' \strong{Corline} \cr
+#' Something about Corline
+#'
+#' \strong{Cordoleani} \cr
+#' Something
+#'
+#' \strong{Zeug} \cr
+#' Something
+#'
+#' \strong{Montgomery} \cr
+#'
+#' \strong{Zooper R library} \cr
+#' Something
+#' \href{https://github.com/InteragencyEcologicalProgram/zooper}
+#'
+#' \strong{Guignard} \cr
+#' Something
+#'
+#' @source
+#' something
+#' @export
+"all_prey_density"
+
+#' All Environmental Data
+#'
+"all_enviro_data"
+
+#' All Fish Data
+#'
+"all_fish_data"
